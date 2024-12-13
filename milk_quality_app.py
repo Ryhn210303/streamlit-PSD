505b704fa9e7d4526aeb66e398eac6649759cc6a
Ryhn210303 <reyhanaditya553@gmail.com>
2024-12-13 23:30:07 +0700

    Update milk_quality_app.py

--- milk_quality_app.py
+++ milk_quality_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
+##from sklearn.preprocessing import LabelEncoder, StandardScaler
+##from sklearn.tree import DecisionTreeClassifier
+##from sklearn.naive_bayes import GaussianNB
+##from sklearn.model_selection import train_test_split
+##from sklearn.metrics import accuracy_score, classification_report
 import matplotlib.pyplot as plt
 import numpy as np
 
